3cc1f664f00d1f71d5130317869dc15671cd3c87
Naresh Koirala <koiralanaresh10@gmail.com>
2025-08-03 09:48:18 -0600

    Intial practice and learning

---
+++ nextjs-blog/pages/posts/[id].js
@@ -0,0 +1,18 @@
+import Layout from "../../components/layout";
+
+
+export default function Post( ) {
+  return (
+    <Layout>
+
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+    
+}
+
+export async function getStaticProps({ params }) {
+
+}
