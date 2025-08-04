574c3d6f2626cff1acb7ebba40b9b593ca4e060d
Naresh Koirala <koiralanaresh10@gmail.com>
2025-08-04 04:44:24 -0600

    feat: enhance blog functionality with new features and components
    - Added date-fns for date formatting.
    - Updated index page to include links to individual posts and formatted dates.
    - Improved post detail page with dynamic routing and structured content display.
    - Created a new Date component for consistent date formatting across the application.
    - Added a simple API route for a greeting message.

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -2,8 +2,10 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Alert from '../components/alert';
-import {getSortedPostsData} from '../lib/posts';
- 
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
@@ -20,25 +22,23 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hi!!! I am Naresh the Anime and Chelsea FREAK!!!!!!!!</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-
-        <Alert type="success">This is a success alert!</Alert>
-        <Alert type="error">This is an error alert!</Alert>
       </section>
+
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
