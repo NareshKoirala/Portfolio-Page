ebaa94b084678ffaf2e7ada4ef6b6bf2f395a9bb
Naresh Koirala <koiralanaresh10@gmail.com>
2025-08-05 17:10:21 -0600

    Add contact and projects pages with basic layout structure

--- portfolio_page/src/pages/contact.tsx
+++ portfolio_page/src/pages/contact.tsx
@@ -2,7 +2,7 @@ import Layout from "../components/layout";
 
 export default function Contact() {
   return (
-    <Layout home>
+    <Layout>
       <h1>Contact Me</h1>
       <p>This is the contact page.</p>
     </Layout>
--- portfolio_page/src/pages/projects.tsx
+++ portfolio_page/src/pages/projects.tsx
@@ -2,7 +2,7 @@ import Layout from "../components/layout";
 
 export default function Projects() {
   return (
-    <Layout home>
+    <Layout>
       <h1>My Projects</h1>
       <p>This is the projects page.</p>
     </Layout>
