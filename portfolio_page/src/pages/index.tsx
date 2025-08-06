68a03db997daecfe2d5be451582236772ffe07f0
Naresh Koirala <koiralanaresh10@gmail.com>
2025-08-05 20:54:07 -0600

    Add @vercel/speed-insights dependency and integrate SpeedInsights component in index page; fix image path casing in about and nav-bar components

--- portfolio_page/src/pages/index.tsx
+++ portfolio_page/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Home from './home';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Index() {
   return (
     <>
       <Home />
+      <SpeedInsights />
     </>
   );
 }
