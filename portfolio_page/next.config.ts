73f5e1624f05c406685dd1b175802e737d909023
Naresh Koirala <koiralanaresh10@gmail.com>
2025-08-06 01:19:07 -0600

    Refactor image optimization settings: move unoptimized config to next.config.ts and remove vercel.json

--- portfolio_page/next.config.ts
+++ portfolio_page/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images:{
+    unoptimized: true // Set to true if you want to use unoptimized images
+  },
 };
 
 export default nextConfig;
